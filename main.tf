115fddaede8f724f0b102034389a6b0f789fbc71
Carmine Fabrizio <carmine.fabrizio@hivemq.com>
2023-03-09 17:24:12 +0100

    First commit: created the tf-module-github and the relative doc

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
-module "hivemq_github" {
-  source         = "./module"
-  name           = "cfabrizio-repo-test"
-  description    = "This is a repo test"
-  auto_init      = true
-  default_branch = "main"
+# module "hivemq_github" {
+#   source         = "./module"
+#   name           = "example-repo-test"
+#   description    = "This is a repo test"
+#   auto_init      = true
+#   default_branch = "main"
 
-  template = {
-    repository           = "sre-repo-template"
-    owner                = "hivemq-cloud"
-    include_all_branches = false
-  }
+#   template = {
+#     repository           = "sre-repo-template"
+#     owner                = "hivemq-cloud"
+#     include_all_branches = false
+#   }
 
-  branches = [
-    {
-      name = "development"
-    },
-    {
-      name = "playground"
-    }
-  ]
+#   branches = [
+#     {
+#       name = "development"
+#     },
+#     {
+#       name = "playground"
+#     }
+#   ]
 
-  admin_team_ids = ["sre"]
+#   admin_team_ids = ["sre"]
 
-}
+# }
